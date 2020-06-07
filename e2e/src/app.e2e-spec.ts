ba8c5038359567140f10a68e3088d39b6f136be8
david.fogas <david.fogas@ibillboard.com>
2020-06-07 12:05:03 +0200

    initial setup

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('terraform-planet app is running!');
+    expect(page.getTitleText()).toEqual('terraform-planet-ui-ui app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'terraform-planet'`, () => {
+  it(`should have as title 'terraform-planet-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('terraform-planet');
+    expect(app.title).toEqual('terraform-planet-ui-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('terraform-planet app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('terraform-planet-ui-ui app is running!');
   });
 });
