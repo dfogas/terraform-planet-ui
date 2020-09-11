368bb33d6d3b64833ffa8d6951f2006e720c7ad9
david.fogas <david.fogas@ibillboard.com>
2020-09-11 13:48:13 +0200

    + mat grid list

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatGridList, MatGridListModule} from '@angular/material/grid-list';
 
-import { GameRoutingModule } from './game-routing.module';
-import { GameComponent } from './game.component';
-import { HexGridComponent } from './hex-grid/hex-grid.component';
+import {GameRoutingModule} from './game-routing.module';
+import {GameComponent} from './game.component';
+import {HexGridComponent} from './hex-grid/hex-grid.component';
 import {ProjectsBuiltComponent} from './projects-built/projects-built.component';
 
 @NgModule({
     declarations: [GameComponent, HexGridComponent, ProjectsBuiltComponent],
     exports: [
-        GameComponent
+        GameComponent,
+        ProjectsBuiltComponent
     ],
     imports: [
         CommonModule,
         GameRoutingModule,
+        MatGridListModule
     ]
 })
-export class GameModule { }
+export class GameModule {
+}
